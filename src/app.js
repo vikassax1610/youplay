302ca8f42ba2526eaee8232eac530629fa9e7b4f
vikassax1610 <vikassaxena816@gmail.com>
2025-09-02 22:40:49 +0530

    feat: implement Express server with MongoDB connection and add cookie-parser and cors dependencies

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import connectDB from "./db/index.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded());
+app.use(express.static("public"));
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import connectDB from "./db/index.js";
+import app from "./app.js"
 import "dotenv/config";
-connectDB();
+const port = process.env.PORT || 8000;
+connectDB()
+  .then(() => {
+    app.listen(port);
+    console.log(`Server is running at ${port}`);
+  })
+  .catch((err) => console.log("mongoDB connection failed", err));

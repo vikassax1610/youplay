5112fb00a35f809a063da017b09e1fed37ce2f24
vikassax1610 <vikassaxena816@gmail.com>
2025-09-07 19:39:59 +0530

    feat: implement user registration endpoint and multer middleware for file uploads; add cloudinary utility for image storage

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,10 @@
 // option 1
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
-export default asyncHandler;
+export  {asyncHandler};
 
 // option 2
 // const asyncHandler = (fn) => async (req, res, next) => {

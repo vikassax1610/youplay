1ed66bac2155a33dc1247a9d6445fe30f4b3b92a
vikassax1610 <vikassaxena816@gmail.com>
2025-09-08 22:39:50 +0530

    fix: correct express.urlencoded middleware usage in app.js; update registerUser controller to handle request body

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(
   })
 );
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "successfully working",
-  });
+  const { userName, email, fullName, password } = req.body;
+  console.log("email", email);
+
 });
 export default registerUser;
